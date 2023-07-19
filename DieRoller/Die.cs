7f342a726651f4d5877e712d042adf84d8e12c7b
wague <wague@WARRENGUERTIN>
2023-07-18 19:13:33 -0700

    Added constructor

--- DieRoller/Die.cs
+++ DieRoller/Die.cs
@@ -12,9 +12,18 @@ namespace DieRoller
     public class Die
     {
         /// <summary>
+        /// Creates the die and rolls it to start
+        /// with a random number 
+        /// </summary>
+        public Die()
+        {
+            Roll();
+        }
+
+        /// <summary>
         /// The current face up value of the die 
         /// </summary>
-        public byte FaceValue { get; set; }
+        public byte FaceValue { get; private set; }
         
         /// <summary>
         /// True if the die is currently held 
